3bff41cb914310c270e452f5d715ef05ad40a13a
giabao0212 <ngducgiabao96@gmail.com>
2016-08-01 11:42:36 +0700

    first commit

---
+++ TuneOnProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuneOnProject
+//
+//  Created by Nguyen Duc Gia Bao on 8/1/16.
+//  Copyright © 2016 Nguyen Duc Gia Bao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
